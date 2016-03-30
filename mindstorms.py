d862b2513a2017cf47c69525f82707ceb976b587
Sadeq <s.sepehrnoush@gmail.com>
2016-03-30 14:14:07 +0430

    Added files via upload

---
+++ mindstorms.py
@@ -0,0 +1,31 @@
+import turtle
+
+def draw_square () :
+    window = turtle.Screen ()
+    window.bgcolor ("red")
+    x = 10
+    y = 50
+    brad = turtle.Turtle ()
+    brad.speed (0.2)
+    while x<500 :
+        
+        
+        
+        brad.forward (y)
+        brad.right (90)
+        brad.forward (x)
+        brad.right (90)
+        brad.forward (y)
+        x = x + 10
+        y = y + 5
+    
+        
+   
+    
+
+    
+    
+    window.exitonclick ()
+
+
+draw_square ()
